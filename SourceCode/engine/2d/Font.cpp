4b46a17521d0471a376b1f4d5a7dee13ffa5c360
koyamakaito <k020g1197@g.neec.ac.jp>
2023-10-24 11:17:19 +0900

    Textマネージャーを使いやすくする。【今期アニメは面白い】

--- SourceCode/engine/2d/Font.cpp
+++ SourceCode/engine/2d/Font.cpp
@@ -3,18 +3,27 @@
 #include <Helper.h>
 
 DirectX::GraphicsMemory* Font::_gmemory = nullptr;
+DirectXCommon* Font::dxCommon = nullptr;
+Font::Font(wchar_t* word, const XMFLOAT2& position, const XMVECTOR& color) {
+	m_StartFlag = true;
+	m_Word = word;
+	m_Length = wcslen(m_Word);
+	m_Position = position;
+	m_Color = color;
+	Initialize();
+}
 
 Font::~Font() {
-	/*delete _spritefont;
-	delete _spritebatch;*/
+
 }
 
-void Font::Initialize(DirectXCommon* dxcommon) {
-	_gmemory = new DirectX::GraphicsMemory(dxcommon->GetDev());
+void Font::Initialize() {
+	LoadFont();
+
 }
 
-void Font::LoadFont(DirectXCommon* dxcommon) {
-	DirectX::ResourceUploadBatch resUploadBatch(dxcommon->GetDev());
+void Font::LoadFont() {
+	DirectX::ResourceUploadBatch resUploadBatch(dxCommon->GetDev());
 
 	resUploadBatch.Begin();
 	DirectX::RenderTargetState rtState(
@@ -22,65 +31,71 @@ void Font::LoadFont(DirectXCommon* dxcommon) {
 		DXGI_FORMAT_D32_FLOAT);
 	DirectX::SpriteBatchPipelineStateDescription pd(rtState);
 
-	_spritebatch = new DirectX::SpriteBatch(dxcommon->GetDev(), resUploadBatch, pd);
+	_spritebatch = std::make_unique<DirectX::SpriteBatch>(dxCommon->GetDev(), resUploadBatch, pd);
 
-	_heapForSpriteFont = dxcommon->CreateDescriptorHeapForSproteFont();
-	_spritefont = new DirectX::SpriteFont(dxcommon->GetDev(),
+	_heapForSpriteFont = dxCommon->CreateDescriptorHeapForSproteFont();
+	_spritefont = std::make_unique<DirectX::SpriteFont>(dxCommon->GetDev(),
 		resUploadBatch,
 		L"Resources/font/keifont.spritefont",
 		_heapForSpriteFont->GetCPUDescriptorHandleForHeapStart(),
 		_heapForSpriteFont->GetGPUDescriptorHandleForHeapStart());
 
-	auto future = resUploadBatch.End(dxcommon->GetQue());
-	dxcommon->GetCmdList()->SetDescriptorHeaps(1, _heapForSpriteFont.GetAddressOf());
+	auto future = resUploadBatch.End(dxCommon->GetQue());
+	dxCommon->GetCmdList()->SetDescriptorHeaps(1, _heapForSpriteFont.GetAddressOf());
 	future.wait();
-	_spritebatch->SetViewport(dxcommon->GetViewPort());
+	_spritebatch->SetViewport(dxCommon->GetViewPort());
 }
 
+void Font::Draw() {
+	dxCommon->GetCmdList()->SetDescriptorHeaps(1, _heapForSpriteFont.GetAddressOf());
+	_spritebatch->Begin(dxCommon->GetCmdList());
+	XMFLOAT2 shadow_pos = {
+		m_Position.x - 2.f,
+		m_Position.y - 2.f
+	};
+	_spritefont->DrawString(_spritebatch.get(), m_Word,
+		shadow_pos, DirectX::Colors::Black);
 
-void Font::StringReset() {
-	m_Word = L" ";
-}
-
-void Font::Draw(DirectXCommon* dxcommon) {
-	dxcommon->GetCmdList()->SetDescriptorHeaps(1, _heapForSpriteFont.GetAddressOf());
-	_spritebatch->Begin(dxcommon->GetCmdList());
-	_spritefont->DrawString(_spritebatch, m_Word,
-		disply_place_, DirectX::Colors::Black, {}, shadow_position_);
-
-	_spritefont->DrawString(_spritebatch, m_Word,
-		disply_place_, color_, {}, position_);
+	_spritefont->DrawString(_spritebatch.get(), m_Word,
+		m_Position, m_Color);
 	_spritebatch->End();
 }
 
-void Font::PostDraw(DirectXCommon* dxcommon) {
-	_gmemory->Commit(dxcommon->GetQue());
+void Font::SetGraphicMemory(DirectXCommon* dxcommon) {
+	dxCommon = dxcommon;
+	_gmemory = new DirectX::GraphicsMemory(dxCommon->GetDev());
 }
 
-void Font::SetString(wchar_t* ward) {
+void Font::PostDraw() {
+	_gmemory->Commit(dxCommon->GetQue());
+}
+
+void Font::SetString(wchar_t* word) {
 	m_StartFlag = true;
-	m_Word = ward;
+	m_Word = word;
 }
 
 bool Font::FlowText() {
+	static float time = 0.f;
+	static wchar_t* m_NowWord_ = L"NULL";
 	if (m_StartFlag == true) {
 		m_NowWord_ = m_Word;
-		len_ = wcslen(m_NowWord_);
+		m_Length = wcslen(m_NowWord_);
 		m_StartFlag = false;
 	}
-	if (len_ == 0) {
+	if (m_Length <= 0) {
 		//テキスト終了
 		return  true;
 	} else {
-		if (Helper::GetInstance()->FrameCheck(time_, 1.0f)) {
-			len_ -= 1;
-			Helper::GetInstance()->Clamp(len_,(size_t)0,(size_t)20);
-			time_ = 0.f;
+		if (Helper::GetInstance()->FrameCheck(time, 1.0f)) {
+			m_Length -= 1;
+			Helper::GetInstance()->Clamp(m_Length, (size_t)0, (size_t)20);
+			time = 0.f;
 		}
 		size_t newSize = wcslen(m_NowWord_);
 		size_t origsize = wcslen(m_NowWord_) + 1;
 		wchar_t* wcstr = new wchar_t[newSize];
-		wcsncpy_s(wcstr, origsize, m_NowWord_, newSize - len_);
+		wcsncpy_s(wcstr, origsize, m_NowWord_, newSize - m_Length);
 		m_Word = wcstr;
 		return false;
 	}
