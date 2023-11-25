31a2e9216ea5580727c8ac18bdbb3bbca3ef31c7
koyamakaito <k020g1197@g.neec.ac.jp>
2023-11-26 05:51:26 +0900

    【カウンターの表示準備】

--- SourceCode/gamesystem/text/Onomatope.cpp
+++ SourceCode/gamesystem/text/Onomatope.cpp
@@ -10,7 +10,7 @@ void (Onomatope::* Onomatope::updateTable[])(OnomatoStruct& onomato) = {
 	&Onomatope::BossSpawnUpdate,//
 	&Onomatope::GameOverUpdate,//
 	&Onomatope::AttackChargeUpdate,//
-
+	& Onomatope::CounterUpdate,
 };
 
 Onomatope::Onomatope() {
@@ -78,6 +78,14 @@ void Onomatope::AddOnomato(OnomatoPattern patten, XMFLOAT2 basePos, float delay)
 		str.kFrameMax = 45.f;
 		str.kDelayFrameMax = delay;
 		str.pattern = AttackCharge;
+		break;
+	case Counter:
+		str.Tex = IKESprite::Create(ImageManager::ONOMATO_05, basePos);
+		str.Tex->SetAnchorPoint({ 0.5f,0.5f });
+		str.Tex->SetSize({ 256.f,256.f });
+		str.kFrameMax = 60.f;
+		str.kDelayFrameMax = delay;
+		str.pattern = Counter;
 		break;
 	default:
 		break;
@@ -163,4 +171,28 @@ void Onomatope::AttackChargeUpdate(OnomatoStruct& onomato) {
 		onomato.isFinish = true;
 	}
 
+}
+
+void Onomatope::CounterUpdate(OnomatoStruct& onomato) {
+	onomato.delayFrame += 1 / onomato.kDelayFrameMax;
+	if (onomato.delayFrame < 1.0f) {
+		return;
+	}
+	onomato.frame += 1 / onomato.kFrameMax;
+
+	if (onomato.frame <= 1.0f) {
+		float alpha = 1.0f;
+		alpha = Ease(In, Quart, onomato.frame, 1.f, 0.f);
+		onomato.Tex->SetColor({ 1.f,1.f,1.f,alpha });
+		XMFLOAT2 siz = onomato.Tex->GetSize();
+		siz.x = Ease(Out, Back, onomato.frame, 256.f, 280.f);
+		siz.y = Ease(Out, Back, onomato.frame, 256.f, 280.f);
+		onomato.Tex->SetSize(siz);
+		XMFLOAT2 pos = onomato.Tex->GetPosition();
+		pos.x = Ease(Out, Quad, onomato.frame, onomato.basePos.x, onomato.basePos.x);
+		pos.y = Ease(Out, Quad, onomato.frame, onomato.basePos.y, onomato.basePos.y - 180.0f);
+		onomato.Tex->SetPosition(pos);
+	} else {
+		onomato.isFinish = true;
+	}
 }
