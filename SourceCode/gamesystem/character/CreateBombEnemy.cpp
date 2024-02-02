727d4db9d3c3c3a603617902c6d7067e3e4bf7c9
koyamakaito <k020g1197@g.neec.ac.jp>
2024-02-02 22:23:52 +0900

    【もとに戻しました】

--- SourceCode/gamesystem/character/CreateBombEnemy.cpp
+++ SourceCode/gamesystem/character/CreateBombEnemy.cpp
@@ -188,14 +188,11 @@ void CreateBombEnemy::Teleport() {
 }
 //”š’e‚Ì¶¬
 void CreateBombEnemy::BirthBomb() {
-	int l_RandWidth;
-	int l_RandHeight;
-	StagePanel::GetInstance()->PoisonSetPanel(l_RandWidth,l_RandHeight);
 	//ƒ{ƒ€¶¬
 	std::unique_ptr<Bomb> newBomb = std::make_unique<Bomb>();
 	newBomb->Initialize();
 	newBomb->SetPosition({ m_Position.x,m_Position.y + 1.2f,m_Position.z });
-	//newBomb->SetTargetPos(StagePanel::GetInstance()->EnemySetPanel(true));
+	newBomb->SetTargetPos(StagePanel::GetInstance()->EnemySetPanel(true));
 	newBomb->SetPlayer(player);
 	bomb.push_back(std::move(newBomb));
 }
